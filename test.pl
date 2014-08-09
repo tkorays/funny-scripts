f47b6244de7bb504bc1d8792f05fae6a04764a57
tkorays <tkorays@hotmail.com>
2014-08-10 05:59:37 +0800

    测试

---
+++ test.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl
+
+push (@INC,'xkxt');
+use xkxt::Whuxk;
+
+
+my $whuer = xkxt::Whuxk->new({id=>'2011301200047',pwd=>'22000414',cookie=>'lwp_cookies.txt'});
+$whuer->conf_baseurl('http://202.114.74.198'); 
+$whuer->get_captcha('a.jpg');
+print 'input captcha:';
+chomp(my $cap=<STDIN>);
+$whuer->set_captcha($cap);
+print $whuer->login;
