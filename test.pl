efc982ed1081fab6101a629545d14e4aef238239
tkorays <tkorays@hotmail.com>
2014-08-10 06:02:20 +0800

    comment

--- test.pl
+++ test.pl
@@ -4,7 +4,7 @@
 use xkxt::Whuxk;
 
 
-my $whuer = xkxt::Whuxk->new({id=>'2011301200047',pwd=>'22000414',cookie=>'lwp_cookies.txt'});
+my $whuer = xkxt::Whuxk->new({id=>'***',pwd=>'***',cookie=>'lwp_cookies.txt'});
 $whuer->conf_baseurl('http://202.114.74.198'); 
 $whuer->get_captcha('a.jpg');
 print 'input captcha:';
